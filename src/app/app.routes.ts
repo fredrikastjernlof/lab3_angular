126250b3e1904e512484f5d7a567e0db81d232b6
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-04-27 11:12:03 +0200

    Feat: add main navigation, routing and 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Convert } from './convert/convert';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: Home},
+    { path: "start", component: Home},
+    { path: "about", component: About},
+    { path: "om", component: About},
+    { path: "convert", component: Convert},
+    { path: "omvandlare", component: Convert},
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "404", component: NotFound},
+    { path: "**", redirectTo: "404", pathMatch: "full"}
+];
---
+++ src/app/main-nav/main-nav.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-main-nav',
+  imports: [RouterLink],
+  templateUrl: './main-nav.html',
+  styleUrl: './main-nav.scss',
+})
+export class MainNav {}
