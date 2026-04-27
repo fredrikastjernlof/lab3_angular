e922dd139da024a9d6d7fc6013dfb0d2af6a46d9
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-04-27 12:36:13 +0200

    Feat: implement unit converter with two-way data binding

--- src/app/convert/convert.ts
+++ src/app/convert/convert.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.html',
   styleUrl: './convert.scss',
 })
-export class Convert {}
+export class Convert {
+
+  meters: number = 0;
+  feet: number = 0;
+
+  celsius: number = 0;
+  fahrenheit: number = 32;
+
+  selectedConverter: string = 'length';
+
+  convertToFeet() {
+    this.feet = +(this.meters * 3.28084).toFixed(2);
+  }
+
+  convertToMeters() {
+    this.meters = +(this.feet / 3.28084).toFixed(2);
+  }
+
+  convertToFahrenheit() {
+    this.fahrenheit = +((this.celsius * 9/5) + 32).toFixed(2);
+  }
+
+  convertToCelsius() {
+    this.celsius = +((this.fahrenheit - 32) * 5/9).toFixed(2);
+  }
+
+}
